ac2a4353f14704122d50919278d4251f79e65194
Paomian <xpaomian@gmail.com>
2017-02-26 17:08:14 +0000

    (feat) fix compile error

--- src/wanjuche.cpp
+++ src/wanjuche.cpp
@@ -19,9 +19,10 @@
 /* Libevent. */
 #include <event2/event.h>
 
+struct event_base *base = event_base_new();
 
 struct client {
-    struct event ev_read;
+    struct event *ev_read;
 };
 
 int set_noblock(int fd) {
@@ -38,20 +39,20 @@ int set_noblock(int fd) {
 }
 
 void on_read(int fd, short ev, void *arg) {
-    struct client * client = (struct client*) arg;
+    struct client* client = (struct client*) arg;
     u_char buf[8196];
     int len,wlen;
     len = read(fd, buf, sizeof(buf));
     if (len == 0) {
         std::cout << "Client sidconnected." << std::endl;
         close(fd);
-        event_del(&client -> ev_read);
+        event_del(client -> ev_read);
         free(client);
         return;
     } else if (len < 0) {
         std::cout << "Socket failure, disconnect client:" << std::endl;
         close(fd);
-        event_del(&client -> ev_read);
+        event_del(client -> ev_read);
         free(client);
         return;
     }
@@ -66,7 +67,7 @@ void on_accept(int fd, short ev, void *arg) {
     int client_fd;
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
-    struct client *client;
+    struct client *cli;
     client_fd = accept(fd, (struct sockaddr*)&client_addr, &client_len);
     if (client_fd == -1) {
         warn("accept failed");
@@ -76,24 +77,20 @@ void on_accept(int fd, short ev, void *arg) {
     if (set_noblock(client_fd) < 0)
         warn("failed to set client socket non-blocking");
 
-    client = calloc(1, sizeof(*client));
-    if (client == nullptr)
+    cli = (client*)std::calloc(1, sizeof(*cli));
+    if (cli == nullptr)
         err(1, "malloc failed");
 
-    event_set(&client->ev_read, client_fd, EV_READ|EV_PERSIST, on_read, client);
+    cli->ev_read = event_new(base,client_fd, EV_READ|EV_PERSIST, on_read, cli);
 
-    event_add(&client -> ev_read, nullptr);
+    event_add(cli -> ev_read, nullptr);
     printf("Accepted connection from %s\n", inet_ntoa(client_addr.sin_addr));
 }
 
 int main(int argc, char* argv[], char* envp[]) {
     int listen_fd;
     struct sockaddr_in listen_addr;
-    int reuseadd_on = 1;
-
-    struct event ev_accept;
-
-    event_init();
+    int reuseaddr_on = 1;
 
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_fd < 0)
@@ -104,18 +101,20 @@ int main(int argc, char* argv[], char* envp[]) {
         err(1, "setsockopt failed");
     std::memset(&listen_addr, 0, sizeof(listen_addr));
     listen_addr.sin_family = AF_INET;
-    listen_addr.sin_addr = INADDR_ANY;
+    listen_addr.sin_addr.s_addr = INADDR_ANY;
     listen_addr.sin_port = htons(8888);
     if (bind(listen_fd, (struct sockaddr*)&listen_addr,sizeof(listen_addr)) < 0)
-        error(1, "bind failed");
+        err(1, "bind failed");
     if (listen(listen_fd,5) < 0)
         err(1, "listen failed");
     if(set_noblock(listen_fd) < 0)
         err(1, "failed to set server socket to non-blocking");
 
-    event_set(&ev_accept,listen_fd,EV_READ|EV_PERSIST, on_accept,nullptr);
-    event_add(&ev_accept,NULL);
-    event_dispatch();
+    struct event *ev_accept;
+    ev_accept = event_new(base,listen_fd,EV_READ|EV_PERSIST,on_accept,nullptr);
+
+    event_add(ev_accept,nullptr);
+    event_base_dispatch(base);
     return 0;
 
 }
